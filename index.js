a87817c690ee184d7262569a30a5f900c91a0352
中上 剛 <tsuyoshi.nakagami@persol.co.jp>
2020-08-05 13:58:21 +0900

    first -commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const db =  document.getElementById('DB');
+const result = document.getElementById('result');
+const clipboard = new ClipboardJS('#btnCopy');
+
+
+db.addEventListener('change', (ev) => {
+  result.innerText = '';
+  const allLineText = ev.target.value;
+  const texts = allLineText.split(/\n/);
+  const generatedText = texts.map(text => {
+    const items = text.split(/\t/);
+    if (items.length < 1) return '';
+
+    const physicalName =  items[1];
+    const logicalName = items[0];
+
+    return `
+    /// <summary>
+    /// ${logicalName}
+    /// </summary>
+    public string ${physicalName} { get; set; }
+    `
+  }).join('\n');
+  result.value = generatedText;
+});
+
+clipboard.on('success', function (e) {
+  e.clearSelection();
+  const information = document.getElementById("information");
+  information.classList.remove('hide');
+  setTimeout(() => {
+    information.classList.add('hide');
+  }, 3000);
+});
